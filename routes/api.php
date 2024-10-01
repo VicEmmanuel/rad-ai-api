eedf99f30fc8e3a757b1da1c00a0115036ce9d56
VicEmmanuel <vicemmanuel7@gmail.com>
2024-10-01 05:25:43 +0100

    first commit

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,13 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-Route::post('login', [AuthenticationController::class, 'login']);
-Route::post('register', [AuthenticationController::class, 'register']);
+//Route::post('login', [AuthenticationController::class, 'login']);
+//Route::post('register', [AuthenticationController::class, 'register']);
 Route::post('resend-otp', [AuthenticationController::class, 'resendOtp']);
 Route::post('verify-otp', [AuthenticationController::class, 'verifyOtp']);
 Route::post('forgot-password', [AuthenticationController::class, 'forgotPassword']);
 Route::post('reset-password', [AuthenticationController::class, 'resetPassword']);
+
+Route::prefix('api')->group(function () {
+    Route::post('login', [AuthenticationController::class, 'login']);
+    Route::post('register', [AuthenticationController::class, 'register']);});
